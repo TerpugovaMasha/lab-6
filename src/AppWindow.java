0420662c6316e6c98326b66d69701a3e3d6d4f2f
TerpugovaMasha <56717263+TerpugovaMasha@users.noreply.github.com>
2020-01-11 14:15:00 +0600

    Update AppWindow.java

--- src/AppWindow.java
+++ src/AppWindow.java
@@ -1,48 +1,43 @@
-
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java . awt . * ;
+import java.awt . event . * ;
+import java . applet .*;
+import java.util.Random;
 
 public class AppWindow extends Frame {
-    public AppWindow(){
+    public AppWindow() {
         addWindowListener(new MyWindowAdapter());
     }
+    public static void main(String args[]) {
+        AppWindow appwin = new AppWindow();
+        appwin.setSize(new Dimension(900, 900));
+        appwin.setTitle("Lab 6");
+        appwin.setVisible(true);
 
-    public  static  void main (String[] args){ //точка входа
-        final int w = 80;
-        final  int h = 30;
-
-        AppWindow appWindow = new AppWindow();
-        appWindow.setSize(400,300);
-        appWindow.setTitle("UnlockedButton"); // название окна
-        appWindow.setVisible(true); //установить видимость
-
-        Button button = new Button("Нажми");
-        button.setBounds(50,100, w, h);
-
+        Button button = new Button();
+        button.setBounds(10, 10, 100, 50);
+        button.setLabel("press me");
+        button.setVisible(true);
         button.addMouseMotionListener(new MouseMotionListener() {
-
-            public void mouseDragged(MouseEvent mouseEvent) {
-
+            @Override
+            public void mouseDragged(MouseEvent e) {
             }
-
-            public void mouseMoved(MouseEvent mouseEvent) {
-                int x = appWindow.getBounds().width - w ;
-                int y = appWindow.getBounds().height - h ;
-                button.setBounds(x, y, w, h);
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                button.setBounds(
+                        (int)(Math.random()*(appwin.getWidth()-button.getWidth())),
+                        (int)(Math.random()*(appwin.getHeight()-button.getHeight())),
+                        button.getWidth(),
+                        button.getHeight());
             }
         });
 
-        appWindow.add(button);
-        appWindow.setLayout(null);
-    }
-
-    class MyWindowAdapter extends WindowAdapter{
-        public void windowClosing(WindowEvent we) {
-            System.exit(0);
-        }
+        appwin.add(button);
+        appwin.setLayout(null);
     }
 }
 
+class MyWindowAdapter extends WindowAdapter {
+    public void windowClosing(WindowEvent we) {
+        System.exit(0);
+    }
+}
