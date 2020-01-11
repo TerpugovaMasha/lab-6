df44a6fb3cc44062d3391f772eb68ca1cece24de
TerpugovaMasha <56717263+TerpugovaMasha@users.noreply.github.com>
2020-01-11 14:15:29 +0600

    Update AppWindow.java

--- src/AppWindow.java
+++ src/AppWindow.java
@@ -1,6 +1,6 @@
-import java . awt . * ;
-import java.awt . event . * ;
-import java . applet .*;
+import java.awt.* ;
+import java.awt.event.* ;
+import java.applet.*;
 import java.util.Random;
 
 public class AppWindow extends Frame {
